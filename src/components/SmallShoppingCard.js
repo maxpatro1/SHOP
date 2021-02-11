32c987714bc30b242af7c123c210b3140c441943
AndreyBS1 <andrey.babenkov.00@gmail.com>
2021-02-11 17:44:13 +0300

    Окна товаров

---
+++ src/components/SmallShoppingCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./styles/SmallShoppingCard.css";
+
+class SmallShoppingCard extends React.Component{
+    render() {
+        return(
+            <div className="productPreview">
+
+                <img src="https://static-sl.insales.ru/r/VCCko_Os1pk/fit/440/0/ce/1/plain/images/products/1/7372/400235724/large_product.jpg" alt="not" className="productPreview-photo"/>
+
+                <div className="productPreview-description">
+                    Description
+                </div>
+
+                <div className="productPreview-price">
+                    Price
+                    <b> 1000 </b>
+                </div>
+
+                <div className="productPreview-button">
+                    Select
+                </div>
+
+            </div>
+        )
+    }
+}
+export default SmallShoppingCard;
