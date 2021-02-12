08cc410bf968885ea05cc59dd7a82e8143a09a98
maxim yarullin <maxim.yarullin@nat.kz>
2021-02-12 18:27:11 +0600

    merge with Andrey_B

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Welcome from "./components/Footer";
+import BigShoppingCard from "./components/BigShoppingCard";
+import SmallShoppingCard from "./components/SmallShoppingCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <BigShoppingCard/>
+          <SmallShoppingCard/>
         <Welcome name={'andrey'}/>
       </header>
     </div>
---
+++ src/components/BigShoppingCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/BigShoppingCard.css"
+
+class BigShoppingCard extends React.Component {
+    render() {
+        return(
+            <div className="promoSlider">
+                <img src="https://static-sl.insales.ru/r/TXKZwRvSjoA/fit/1408/0/ce/1/plain/files/1/5229/15242349/original/banner.jpg@webp" alt="not" className="promoSlider-photo"/>
+                <button className="promoSlider-button">  </button>
+            </div>
+        )
+    }
+}
+export default BigShoppingCard; 
